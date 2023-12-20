056cb3e17ccd6eae21dd483f3eab7364168f9db2
Satyen Subramaniam <66653384+SubramaniamSatyen@users.noreply.github.com>
2023-12-19 23:33:55 -0700

    Adding basic firebase access code

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
+import db from "./firebase";
+import { collection, getDocs } from "firebase/firestore";
 
 function App() {
+	async function getCollectionDocs(collectionName) {
+		const collectionRef = collection(db, collectionName);
+		const snapshot = await getDocs(collectionRef);
+		if (snapshot.empty) {
+		  console.log('No matching documents.');
+		  return;
+		}  
+		
+		snapshot.forEach(doc => {
+		  console.log(doc.id, '=>', doc.data());
+		});
+	}
+
 	return (
 		<div>
 			<h1>Strovo</h1>
 			<p>A completely original idea for a running mileage tracker</p>
 			<p>To be implemented :)</p>
+
+			<button onClick={() => getCollectionDocs('animals')}>Print Docs...</button>
+			{/* {currAnimals.map(animal => <div>{animal.animal}</div>)} */}
 		</div>
 	);
 }
