ccbc3b41a46933d339eb0a306310c96ea73ba542
jameswu <jamesw0462@gmail.com>
2023-12-19 17:57:14 -0800

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+	return (
+		<div>
+			<h1>Strovo</h1>
+			<p>A completely original idea for a running mileage tracker</p>
+			<p>To be implemented :)</p>
+		</div>
+	);
+}
+
+export default App;
