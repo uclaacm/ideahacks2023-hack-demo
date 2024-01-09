f34d0c0af5ccb7135632441812b78d31edd2b002
Satyen Subramaniam <66653384+SubramaniamSatyen@users.noreply.github.com>
2024-01-08 17:33:24 -0800

    Whoops...

--- src/firebase.js
+++ src/firebase.js
@@ -8,18 +8,18 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCvOErD6U6Jbnyb_82zi4BvLoQo2VvaBag",
-  authDomain: "straba-88665.firebaseapp.com",
-  projectId: "straba-88665",
-  storageBucket: "straba-88665.appspot.com",
-  messagingSenderId: "610821342416",
-  appId: "1:610821342416:web:ce94a09ae3ff848d558ad5",
-  measurementId: "G-2NDRLD3PV4"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 
-const db = getFirestore();
+const db = getFirestore(app);
 export default db;
